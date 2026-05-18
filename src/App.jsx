679f07081818bf832f5baa4e6cef556fc2ac5830
MariaHasanChowdhury <mariahasanchowdhury1130@gmail.com>
2026-05-18 19:54:37 +0600

    fixed some issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,17 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AppProvider } from './context/AppContext';
-import { Toaster } from 'react-hot-toast';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import FriendDetails from './pages/FriendDetails';
-import Timeline from './pages/Timeline';
-import Stats from './pages/Stats';
-import NotFound from './pages/NotFound';
+import { Outlet } from 'react-router-dom'
+import './App.css'
+import Navbar from './Components/Navbar/Navbar'
 
 function App() {
+
   return (
-    <AppProvider>
-      <Router>
-        <div className="min-h-screen bg-slate-50/50 flex flex-col text-gray-800 font-sans antialiased">
-          {/* Navbar */}
-          <Navbar />
-          
-          {/* Main App Screens */}
-          <main className="flex-grow">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/friend/:id" element={<FriendDetails />} />
-              <Route path="/timeline" element={<Timeline />} />
-              <Route path="/stats" element={<Stats />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </main>
-
-          {/* Toast Notification Container */}
-          <Toaster position="bottom-right" reverseOrder={false} />
-
-          {/* Footer */}
-          <Footer />
-        </div>
-      </Router>
-    </AppProvider>
-  );
+    <>
+      <div className="bg-white min-h-screen"  >
+        {/* <Navbar /> */}
+        {/* <Outlet /> */}
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
